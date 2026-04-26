5bef5d195fdfcaf02330590850bf03cc472f7bf2
Hossein Nazari <hossein.nazari.computer01@gmail.com>
2026-04-26 14:21:09 +0330

    Create Game.py
    Create a Python Project GAME Sang- Kaghaz - Gheychi

---
+++ Game.py
@@ -0,0 +1,12 @@
+computer="Sang"
+user=input("Sang - Kaghaz - gheychi: ")
+if user == computer:
+    print("Mossaavi Shode!")
+elif user == "Kaghaz" and computer== "Sang":
+    print("To Bordi!")
+elif user == "Gheychi" and computer== "Kaghaz":
+    print("To Bordi!")
+elif user == "Sang" and computer== "Gheychi":
+    print("To Bordi!")
+else:
+    print("Computer Bord!") 
