72bcedff361f497841fe6434e3a6d93b72c80ab2
Hossein Nazari <hossein.nazari.computer01@gmail.com>
2026-04-27 11:51:16 +0330

    Update Game.py

--- Game.py
+++ Game.py
@@ -2,11 +2,11 @@ computer="Sang"
 user=input("Sang - Kaghaz - gheychi: ")
 if user == computer:
     print("Mossaavi Shode!")
-elif user == "Kaghaz" and computer== "Sang":
+elif user == "Kaghaz" and computer == "Sang":
     print("To Bordi!")
-elif user == "Gheychi" and computer== "Kaghaz":
+elif user == "Gheychi" and computer == "Kaghaz":
     print("To Bordi!")
-elif user == "Sang" and computer== "Gheychi":
+elif user == "Sang" and computer == "Gheychi":
     print("To Bordi!")
 else:
     print("Computer Bord!") 
